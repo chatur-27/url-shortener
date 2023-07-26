7ded636567220e170d97b5a9e41d733d074d07e7
chatur <chaturreigns@gmail.com>
2023-07-26 20:51:45 +0530

    database code altered

--- server/database.js
+++ server/database.js
@@ -5,19 +5,59 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'url_shortener',
 });
 
+// Connect to MySQL
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to MySQL:', err);
   } else {
     console.log('Connected to MySQL');
-    const deleteAllQuery = 'delete FROM urls';
-    connection.query(deleteAllQuery, (err, results) => {
+
+    // Create the database if it doesn't exist
+    const createDatabaseQuery = 'CREATE DATABASE IF NOT EXISTS url_shortener';
+    connection.query(createDatabaseQuery, (err) => {
       if (err) {
-        console.error('Error delete shortened URLs from MySQL:', err);
-        
+        console.error('Error creating database:', err);
+        connection.end(); // Close the connection
+      } else {
+        console.log('Database created or already exists');
+
+        // Use the created database
+        const useDatabaseQuery = 'USE url_shortener';
+        connection.query(useDatabaseQuery, (err) => {
+          if (err) {
+            console.error('Error using database:', err);
+            connection.end(); // Close the connection
+          } else {
+            console.log('Using database: url_shortener');
+
+            // Create the table if it doesn't exist
+            const createTableQuery = `CREATE TABLE IF NOT EXISTS urls (
+              id INT AUTO_INCREMENT PRIMARY KEY,
+              original_url VARCHAR(255) NOT NULL,
+              short_url VARCHAR(10) NOT NULL
+            )`;
+            connection.query(createTableQuery, (err) => {
+              if (err) {
+                console.error('Error creating table:', err);
+              } else {
+                console.log('Table "urls" created or already exists');
+              }
+
+            });
+
+            // delete all records from table
+            const deleteQuery = `delete from urls;`;
+            connection.query(deleteQuery, (err) => {
+              if (err) {
+                console.error('Error creating table:', err);
+              } else {
+                console.log('delted all records');
+              }
+            });
+          }
+        });
       }
     });
   }
@@ -34,4 +74,3 @@ export const executeQuery = (query, values) => {
       });
     });
   };
-
